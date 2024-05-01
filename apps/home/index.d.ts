05766d285c53887a4cf3337ab8808d3818ea0015
Alessandra Canuto <ale.canutto@gmail.com>
2024-05-01 11:11:04 -0300

    feat(home): add checkout page

--- apps/home/index.d.ts
+++ apps/home/index.d.ts
@@ -4,3 +4,7 @@ declare module '*.svg' {
   export const ReactComponent: any;
   export default content;
 }
+
+declare module '@mfe/checkout/Header'
+
+declare module '@mfe/checkout/pages/index'
---
+++ apps/home/src/pages/checkout/index.tsx
@@ -0,0 +1,13 @@
+import Header from '@mfe/checkout/Header';
+import Index from '@mfe/checkout/pages/index';
+
+const CheckoutPage = () => {
+  return (
+    <>
+      <Header />
+      <Index />
+    </>
+  );
+};
+
+export default CheckoutPage;
