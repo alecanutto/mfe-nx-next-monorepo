477ddc6b6bdecbd5acbfb90fc8a26033dd1f6d7a
Alessandra Canuto <ale.canutto@gmail.com>
2024-10-08 10:34:33 -0300

    Revert "feat: config rspack"
    This reverts commit 42babebe0bb8359be37886281ccd47ece04d3080.

--- apps/checkout/rspack.config.js
+++
@@ -1,6 +0,0 @@
-const { composePlugins, withNx, withReact } = require('@nx/rspack')
-
-module.exports = composePlugins(withNx(), withReact(), (config) => {
-  // Add your custom webpack config here
-  return config
-})
